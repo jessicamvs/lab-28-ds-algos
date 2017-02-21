a73e4802d48cfdf2d8a2115f18ce3951c908ee3f
Jessica Soltero <jmichellevs@gmail.com>
2017-02-20 16:57:50 -0800

    Wrote some insert tests, all passing

--- lab-jessica/test/bst-test.js
+++ lab-jessica/test/bst-test.js
@@ -1,1 +1,55 @@
 
+'use strict';
+
+const expect = require('chai').expect;
+const BST = require('../lib/bst.js');
+
+describe('Binary Search Tree', function() {
+  let bst;
+
+  beforeEach(() => {
+    bst = new BST();
+  });
+
+  describe('#insert()', function() {
+    it('should create a new node as root when tree is empty and return val', function() {
+      expect(bst.root).to.equal(null);
+      expect(bst.insert(5)).to.equal(5);
+      expect(bst.root.val).to.equal(5);
+    });
+
+    it('should add 10 to the right of 5', function() {
+      bst.insert(5);
+      expect(bst.insert(10)).to.equal(10);
+      expect(bst.root.right.val).to.equal(10);
+    });
+
+    it('should add 5 to the right of 5', function() {
+      bst.insert(5);
+      expect(bst.insert(5)).to.equal(5);
+      expect(bst.root.right.val).to.equal(5);
+      expect(bst.root.left).to.equal(null);
+    });
+
+    it('should add 3 to the left of 5', function() {
+      bst.insert(5);
+      expect(bst.insert(3)).to.equal(3);
+      expect(bst.root.left.val).to.equal(3);
+      expect(bst.root.right).to.equal(null);
+    });
+  });
+
+  describe('#remove()', function() {
+    it('', function() {
+
+    });
+
+    it('', function() {
+
+    });
+
+    it('', function() {
+
+    });
+  });
+});
