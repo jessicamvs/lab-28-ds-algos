b6288f7f4d395f89221df8cc94572fc33e0ecee0
Jessica Soltero <jmichellevs@gmail.com>
2017-02-23 16:30:46 -0800

    Super ugly, needs to be refactored super bads, remove only deletes root atm

--- lab-jessica/lib/bst.js
+++ lab-jessica/lib/bst.js
@@ -36,4 +36,80 @@ Tree.prototype.insert = function(val) {
 };
 
 Tree.prototype.remove = function(val) {
+  if(!this.root) return false;
+
+  if(this.root.val === val) {
+
+    if(this.root.right) {
+      let prev = this.root;
+      let curr = this.root.right;
+
+      while(curr.left) {
+        prev = curr;
+        curr = curr.left;
+      }
+      this.root.val = curr.val;
+
+      if(!curr.right) {
+        if(prev.left === curr) {
+          prev.left = null;
+          return;
+        }
+        prev.right = null;
+        return;
+
+      } else if(curr.right) {
+        if(prev.left === curr) {
+          prev.left = curr.right;
+          return;
+        }
+        prev.right = curr.right;
+        return;
+      }
+    } else if(this.root.left) {
+      let prev = this.root;
+      let curr = this.root.left;
+
+      while(curr.right) {
+        prev = curr;
+        curr = curr.right;
+      }
+      this.root.val = curr.val;
+
+      if(!curr.left) {
+        if(prev.right === curr) {
+          prev.right = null;
+          return;
+        }
+        prev.left = null;
+        return;
+      } else if(curr.left) {
+        if(prev.right === curr) {
+          prev.right = curr.left;
+          return;
+        }
+        prev.left = curr.left;
+        return;
+      }
+    }
+
+    this.root = null;
+  }
+
 };
+
+let bst = new Tree();
+bst.insert(9);
+bst.insert(5);
+// bst.insert(12);
+bst.insert(2);
+bst.insert(8);
+bst.insert(6);
+// bst.insert(10);
+// bst.insert(16);
+// bst.insert(11);
+// bst.insert(7);
+bst.remove(9);
+console.log('FULL TREE', bst);
+console.log('right', bst.root.right);
+console.log('left', bst.root.left);
